3ee653c6becd60f03bf91590b0ce9c4b2266f92a
jekaplugarev <jeka_plugarev@mail.ru>
2021-07-24 16:03:20 +0800

    finish 13 lesson

--- src/stories/todolists-api.stories.tsx
+++ src/stories/todolists-api.stories.tsx
@@ -1,18 +1,14 @@
 import React, {useEffect, useState} from 'react'
-import axios from 'axios';
+import {todolistApi} from '../api/todolist-api';
 
 export default {
     title: 'API'
 }
 
-const settings = {
-    withCredentials: true
-}
-
 export const GetTodolists = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
-        axios.get('https://social-network.samuraijs.com/api/1.1/todo-lists', settings)
+        todolistApi.getTodos()
             .then((res) => {
                 setState(res.data);
             })
@@ -24,20 +20,38 @@ export const GetTodolists = () => {
 export const CreateTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const title = 'Hello'
+        todolistApi.createTodo(title)
+            .then((res) => {
+                setState(res.data);
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
+
 export const DeleteTodolist = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todolistId = 'f4a531f4-6d81-4433-becb-d83ebf8eded5';
+        todolistApi.deleteTodo(todolistId)
+            .then((res) => {
+            setState(res.data);
+        })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
 }
+
 export const UpdateTodolistTitle = () => {
     const [state, setState] = useState<any>(null)
     useEffect(() => {
+        const todolistId = 'f4a531f4-6d81-4433-becb-d83ebf8eded5'
+        const title = 'Axios'
+        todolistApi.updateTodoTitle(todolistId, title)
+            .then((res) => {
+                setState(res.data)
+            })
     }, [])
 
     return <div>{JSON.stringify(state)}</div>
